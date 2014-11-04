70b3a84d9493f60f2f177060bed2300848f46ac3
londusboub972 <nolan.jean972@gmail.com>
2014-11-04 16:53:36 +0100

    initiate mate to shell

--- deploygit.sh
+++ deploygit.sh
@@ -1,3 +1,5 @@
+git add -i
+git pull
 echo "All file untracked will be ignored !!" #Just useless line
 git commit -a #Here you're commit all changes done to your project, don't forget that's not adding untracted files
 while true; do #initializing the yn question for pushing or not
