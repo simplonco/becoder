660c4b77c342cf75a12b83956c81d833ebaef6e9
londusboub972 <nolan.jean972@gmail.com>
2014-11-04 17:00:42 +0100

    mod sort

--- deploygit.sh
+++ deploygit.sh
@@ -1,5 +1,5 @@
-git add -i
 git pull
+git add -i
 echo "All file untracked will be ignored !!" #Just useless line
 git commit -a #Here you're commit all changes done to your project, don't forget that's not adding untracted files
 while true; do #initializing the yn question for pushing or not
