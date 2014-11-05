7cc8b0c0fbc7d459567c33927e5ac61cd7f089b2
londusboub972 <nolan.jean972@gmail.com>
2014-11-05 13:02:52 +0100

    deploy to appdesk master now (banch)

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git commit -a #Here you're commit all changes done to your project, don't forget
 while true; do #initializing the yn question for pushing or not
     read -p "Do you want to push it now ?" yn #Easy read -p line
     case $yn in #Expected answers in case yn
-        [Yy]* ) git push origin master && exit;;
+        [Yy]* ) git push appdesk master && exit;;
         [Nn]* ) echo "All done think to push it bro! " && exit;;
         * ) echo "Expected Yes(Yy) or No(Nn)";; #if everything else that the expected Yn question
     esac #End case 
