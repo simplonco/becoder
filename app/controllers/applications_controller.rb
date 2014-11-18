eee69974c3c572a5506bd2fc773b9c85608a3a3d
Anne-Marie Esteves <annemariee@free.fr>
2014-11-18 14:37:24 +0100

    mise à jour du css, ajout d'un menu dans le  layout, nouveau logo ajouté, création d'un formulaire pour créer des users, essai pour que le formulaire fonctionne en suivant le tuto ruby guides mais ça coinde encore

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,6 +4,23 @@ class ApplicationsController < ApplicationController
   end
   
   def create
+    @application = Application.new(params[application_params])
+    @application.save
+    redirect_to @application
   end
 
+  def show
+    @application = Application.find(params[:id])
+  end
+
+  def index
+    @application= Application.all
+  end
+ 
+private
+  def application_params
+    params.require(:application).permit(:why_us, :computer_experiences)
+  end
+
+  
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+  
+  def create
+    @user = User.new(params[user_params])
+    @user.save
+    redirect_to @user
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @user = User.all
+  end
+ 
+private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :birthdate, :birth_place, :address_1, :address_2, :zipcode, :city, :phone_number, :last_degree, :nationality)
+
+  end
+ 
+end
