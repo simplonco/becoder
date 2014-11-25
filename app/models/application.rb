005a8f44dfe8d8df0e681a29b4c299f375ef2bef
FrenchFlakito <guillaume01.simplon@gmail.com>
2014-11-25 16:58:46 +0100

    Adds email application (candidature) confirmation.

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,6 +1,10 @@
 class Application  < ActiveRecord::Base
 	validates :why_us, presence: true,
-                    length: { minimum: 5 }
-    validates :computer_experiences, presence: true,
-                    length: { minimum: 5 }        
+	length: { minimum: 5 }
+	validates :computer_experiences, presence: true,
+	length: { minimum: 5 }
+	validates :email, presence: true,
+	length: { minimum: 5 }
+	validates :name, presence: true,
+	length: { minimum: 5 }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,22 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :sendmail
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'guillaume01.simplon@gmail.com'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            ENV['MAILER_USERNAME'],
+  password:             ENV['MAILER_PWD'],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
+
+
 end
