1d9aaa872306f729ed6eebf9a19ddcc50b51c166
FrenchFlakito <guillaume01.simplon@gmail.com>
2014-11-27 12:52:17 +0100

    Ajout dotenv, amélioration emails, traduction formulaire

--- app/mailers/applicant_mailer.rb
+++ app/mailers/applicant_mailer.rb
@@ -3,6 +3,6 @@ class ApplicantMailer < ActionMailer::Base
 
   def confirmation_email(applicant)
   	@applicant = applicant
-  	mail(to: applicant.email, subject: 'test pour voir')
+  	mail(to: @applicant.email, subject: 'Simplon.com - Confirmation de candidature')
   end
 end
